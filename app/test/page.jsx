8de4fb250bded959e1d5bb3b09ee92ed63f04a5b
aivaneezy1 <aivaneezydakii@gmail.com>
2024-07-11 19:05:52 +0200

    Fixed vercel error

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import BlueDakiModel from '../components/CardModels/BlueDaki'
-import CardMarilyn from '../components/CardDisplay/CardThreeDisplay'
+
 const page = () => {
   return (
-    <div className='flex h-screen'>
-      <div className='w-1/2 bg-blue-500 flex flex-col'>
-        test
-      </div>
+    // <div className='flex h-screen'>
+    //   <div className='w-1/2 bg-blue-500 flex flex-col'>
+    //     This is a test directory nothing special here.
 
-      <div className='w-1/2'>
-        <CardMarilyn/>
-         </div>
+    //   </div>
+
+    //   <div className='w-1/2'>
+     
+    //      </div>
+    // </div>
+
+    <div className='flex justify-center items-center  min-h-screen'>
+    <div className='text-4xl font-bold'>
+      <h2>This is a test directory nothing special here</h2>
+    </div>
     </div>
   )
 }
