2304a874e34f629a49513d26cb5cde0712617f44
aivaneezy1 <aivaneezydakii@gmail.com>
2024-07-11 22:07:36 +0200

    vercel error pt10

--- app/create-cv/page.jsx
+++ app/create-cv/page.jsx
@@ -1,12 +1,15 @@
 "use client"
 import CreateCv from "../components/CreateCv"
+import ClientOnly from "../components/ClientOnly"
+
 
 const CreateCvpage = () => {
   return (
     <div>
-      <CreateCv/>
+      <ClientOnly>
+        <CreateCv/>
+      </ClientOnly>
     </div>
   )
 }
-
 export default CreateCvpage
